9df4f7d2cb9f81b012c98e2a65b74867ee4e6d8c
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-21 15:14:48 +0900

    Initialize

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = this.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder
+                        .setTitle(R.string.dialog_title)
+                        .setMessage(R.string.dialog_message)
+                        .show();
+            }
+        });
+    }
+}
