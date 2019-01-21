501be602cf7b3424d7111a6d30b26c5dcba1a000
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-21 17:44:30 +0900

    Complete

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,26 +3,46 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button execCalcBMI; //計算ボタン
+    private EditText input_height; //身長の入力
+    private EditText input_weight; //体重の入力
+    private TextView bmi_result; //BMIの算出結果
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = this.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        execCalcBMI = findViewById(R.id.button_exec_calc);
+        input_height = findViewById(R.id.input_height_view);
+        input_weight = findViewById(R.id.input_weight_view);
+        bmi_result = findViewById(R.id.text_BMI_view);
+
+        execCalcBMI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-                builder
-                        .setTitle(R.string.dialog_title)
-                        .setMessage(R.string.dialog_message)
-                        .show();
+                double input_height_double = Double.valueOf(input_height.getText().toString()); //身長の取得
+                double input_weight_double = Double.valueOf(input_weight.getText().toString()); //体重の取得
+                double BMI = calcBMI(input_height_double, input_weight_double); //BMIの取得
+                String result = String.format("%.1f", BMI);
+                bmi_result.setText(result);
             }
         });
+    }
+
+    // BMIの計算式
+    private double calcBMI(double height, double weight){
+        double bmi = 0;
+        if(weight > 0 && height > 0) {
+            bmi = weight / (height * height) * 10000;
+        }
+        return bmi;
     }
 }
